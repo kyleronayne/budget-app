9a6453a6726b00857c04a543141ff149e16ef5da
Kyle Ronayne <kyle.ronayne@icloud.com>
2024-01-20 22:14:12 -0500

    Fix month picker animation and logic

--- Budget/BudgetTab/ViewModels/BudgetTabViewModel.swift
+++ Budget/BudgetTab/ViewModels/BudgetTabViewModel.swift
@@ -9,23 +9,36 @@ import Foundation
 
 /// A class that manages the data, user interaction, and presentation logic for a ``BudgetTabView``
 class BudgetTabViewModel: ObservableObject {
-    @Published var selectedBudgetMonth: DateComponents = {
-        Calendar.current.dateComponents([.year, .month], from: Date())
-    }()
+    @Published var selectedBudgetMonth: DateComponents
     @Published var isShowingBudgetCreationView = false
     
-    static let dateFormatter: DateFormatter = {
+    init() {
+        selectedBudgetMonth = Calendar.current.dateComponents([.year, .month], from: Date())
+    }
+    
+    static private let dateFormatter: DateFormatter = {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "MMMM yyyy"
 
         return dateFormatter
     }()
     
-    var budgetMonths: [DateComponents] {
+    var selectedBudgetMonthString: (month: String, year: String) {
+        let monthString = makeMonthString(from: selectedBudgetMonth)
+        let monthStringWords: [String] = monthString.components(separatedBy: " ")
+        
+        if monthStringWords.count == 2 {
+            return (monthStringWords[0], monthStringWords[1])
+        } else {
+            return ("", "")
+        }
+    }
+    
+    var availableBudgetMonths: [DateComponents] {
         var months: [DateComponents] = []
         let today = Date()
         
-        // Generate months +/- 1 year from today
+        // Create months +/- 1 year from today
         for monthIndex in -12...12 {
             if let month = Calendar.current.date(byAdding: .month, value: monthIndex, to: today) {
                 let monthDateComponents = Calendar.current.dateComponents([.year, .month], from: month)
@@ -36,31 +49,16 @@ class BudgetTabViewModel: ObservableObject {
         return months
     }
     
-    /// Creates an attributed month string from the provided date components
+    /// Creates a string for the month represented by the provided date components
     ///
     /// - Parameter month: Date components containing year and month properties
     ///
-    /// - Returns: An attributed month string
-    func makeAttributedMonthString(from month: DateComponents) -> AttributedString {
+    /// - Returns: A month string
+    func makeMonthString(from month: DateComponents) -> String {
         guard let dateForMonth = Calendar.current.date(from: month) else {
             return ""
         }
         
-        let rawMonthString = BudgetTabViewModel.dateFormatter.string(from: dateForMonth)
-        let rawMonthStringWords: [String] = rawMonthString.components(separatedBy: " ")
-        var attributedMonthString = AttributedString(rawMonthString)
-
-        guard let monthStringRange = attributedMonthString.range(of: rawMonthStringWords[0]) else {
-            return ""
-        }
-        
-        guard let yearStringRange = attributedMonthString.range(of: rawMonthStringWords[1]) else {
-            return ""
-        }
-        
-        attributedMonthString[monthStringRange].font = .title2.bold()
-        attributedMonthString[yearStringRange].font = .caption
-        
-        return attributedMonthString
+        return BudgetTabViewModel.dateFormatter.string(from: dateForMonth)
     }
 }
--- Budget/BudgetTab/Views/Components/MonthPicker.swift
+++ Budget/BudgetTab/Views/Components/MonthPicker.swift
@@ -10,55 +10,55 @@ import SwiftUI
 /// A view that expands and collapses to show a wheel-style month picker
 struct MonthPicker: View {
     @EnvironmentObject private var viewModel: BudgetTabViewModel
-    @State private var isShowingPicker = false
+    @State private var isExpanded = false
+    private let chevronImageName = "chevron.down"
+    private let pickerAccessibilityLabelName = "Month"
+    
+    private var labelPadding: Edge.Set {
+        isExpanded ? [.top, .horizontal] : [.all]
+    }
     
     private var chevronDegrees: CGFloat {
-        isShowingPicker ? -180 : 0
+        isExpanded ? -180 : 0
     }
     
-    private var pickerHeight: CGFloat {
-        isShowingPicker ? 150 : 0
-    }
-    
-    private var pickerOpacity: CGFloat {
-        isShowingPicker ? 1 : 0
-    }
-    
-    private var contentPadding: Edge.Set {
-        isShowingPicker ? [.top, .horizontal] : [.all]
+    private var pickerHeight: CGFloat? {
+        isExpanded ? nil : 0
     }
     
     var body: some View {
         VStack(alignment: .leading, spacing: 0) {
-            HStack {
-                Button(action: {
-                    withAnimation(.easeInOut) {
-                        isShowingPicker.toggle()
-                    }
-                }) {
-                    HStack(alignment: .firstTextBaseline) {
-                        Text(viewModel.makeAttributedMonthString(from: viewModel.selectedBudgetMonth))
-                        
-                        Image(systemName: "chevron.down")
-                            .font(.callout)
-                            .rotationEffect(Angle(degrees: chevronDegrees))
-                            .animation(.easeInOut, value: isShowingPicker)
-                    }
+            Button(action: {
+                withAnimation(.easeInOut) {
+                    isExpanded.toggle()
+                }
+            }) {
+                HStack(alignment: .firstTextBaseline) {
+                    Text("\(viewModel.selectedBudgetMonthString.month) ")
+                        .font(.largeTitle.bold())
+                    +
+                    Text(viewModel.selectedBudgetMonthString.year)
+                        .font(.headline.monospaced())
+                    
+                    Image(systemName: chevronImageName)
+                        .font(.headline)
+                        .rotationEffect(Angle(degrees: chevronDegrees))
+                        .animation(.easeInOut, value: isExpanded)
                 }
             }
+            .padding(labelPadding)
             
-            Picker("Month", selection: $viewModel.selectedBudgetMonth) {
-                ForEach(viewModel.budgetMonths, id: \.self) { month in
-                    Text(String(viewModel.makeAttributedMonthString(from: month).characters[...]))
+            Picker(pickerAccessibilityLabelName, selection: $viewModel.selectedBudgetMonth) {
+                ForEach(viewModel.availableBudgetMonths, id: \.self) { month in
+                    Text(viewModel.makeMonthString(from: month))
                         .tag(month)
                 }
             }
             .pickerStyle(.wheel)
             .labelsHidden()
-            .frame(height: pickerHeight)
-            .opacity(pickerOpacity)
+            .frame(height: pickerHeight, alignment: .bottom)
+            .clipped()
         }
-        .padding(contentPadding)
         .background(.white)
     }
 }
